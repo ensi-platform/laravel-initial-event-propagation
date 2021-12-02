a362d872740c550d6d8464306d8ee533e08404ad
arrilot <nekrasov@greensight.ru>
2021-12-02 15:30:20 +0300

    add phpdocs

--- src/InitialEventHolderFacade.php
+++ src/InitialEventHolderFacade.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @method static \Ensi\InitialEventPropagation\InitialEventHolder getInstance()
  * @method static void resetInstances()
- * @method static \Ensi\InitialEventPropagation\InitialEventHolder setInitialEvent(?\Ensi\InitialEventPropagation\InitialEventDTO $initialEvent)
+ * @method static \Ensi\InitialEventPropagation\InitialEventHolder setInitialEvent(\Ensi\InitialEventPropagation\InitialEventDTO|null $initialEvent)
  * @method static \Ensi\InitialEventPropagation\InitialEventDTO getInitialEvent()
  *
  * @see \Ensi\InitialEventPropagation\InitialEventHolder
