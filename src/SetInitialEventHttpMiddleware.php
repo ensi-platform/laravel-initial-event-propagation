0e1d7d7f0b61ba0c43e18b56e4b1eb816b6be505
Наталия <ms.natali.94@yandex.ru>
2023-04-12 13:49:58 +0300

    #106342 (#4)
    * #106342
    
    ---------
    
    Co-authored-by: Ms_Natali <golubkova@greensight.ru>

--- src/SetInitialEventHttpMiddleware.php
+++ src/SetInitialEventHttpMiddleware.php
@@ -12,18 +12,23 @@ class SetInitialEventHttpMiddleware
 {
     public function handle(Request $request, Closure $next): mixed
     {
-        $user = $request->user();
         $config = config('initial-event-propagation', []);
         $mc = config('initial-event-propagation.set_initial_event_http_middleware', []);
         $holder =  Container::getInstance()->make(InitialEventHolder::class);
         $existingEvent = $holder->getInitialEvent();
 
         if (!$existingEvent || empty($mc['preserve_existing_event'])) {
+
+            $user = null;
+            if ($mc['try_auth'] ?? true) {
+                $user = $request->user();
+            }
+
             $initialEvent = InitialEventDTO::fromScratch(
-                userId: $user ? $user->getAuthIdentifier() : "",
-                userType: $user ? $mc['default_user_type'] : "",
                 app: !empty($mc['app_code_header']) ? $request->header($mc['app_code_header'], '') : ($config['app_code'] ?? ''),
                 entrypoint: $this->extractEntrypoint($request),
+                userId: $user ? $user->getAuthIdentifier() : "",
+                userType: $user ? $mc['default_user_type'] : "",
                 correlationId: !empty($mc['correlation_id_header']) ? $request->header($mc['correlation_id_header'], '') : '',
                 timestamp: !empty($mc['timestamp_header']) ? $request->header($mc['timestamp_header'], '') : ''
             );
