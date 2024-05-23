e9d311d68b379d907cc233681596cfde569e2a60
Ms_Natali <golubkova@greensight.ru>
2024-05-23 12:09:26 +0300

    ENSITECH-74

--- src/LaravelInitialEventPropagationServiceProvider.php
+++ src/LaravelInitialEventPropagationServiceProvider.php
@@ -9,26 +9,20 @@ class LaravelInitialEventPropagationServiceProvider extends ServiceProvider
 {
     public const CONFIG_FILE_NAME = 'initial-event-propagation.php';
 
-    /**
-     * @return void
-     */
-    public function register()
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__ . '/../config/' . self::CONFIG_FILE_NAME => config_path(self::CONFIG_FILE_NAME),
+        ]);
+    }
+
+    public function register(): void
     {
         $this->mergeConfigFrom(
             __DIR__ . '/../config/' . self::CONFIG_FILE_NAME,
             'initial-event-propagation'
         );
 
-        $this->app->singleton(InitialEventHolder::class, fn () => new InitialEventHolder());
-    }
-
-    /**
-     * @return void
-     */
-    public function boot()
-    {
-        $this->publishes([
-            __DIR__ . '/../config/' . self::CONFIG_FILE_NAME => config_path(self::CONFIG_FILE_NAME),
-        ]);
+        $this->app->scoped(InitialEventHolder::class, fn () => new InitialEventHolder());
     }
 }
